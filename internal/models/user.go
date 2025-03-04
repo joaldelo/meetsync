35b8580a5e24750d010e180b9e1d60b23c22522e
Joaldelo <jose@joaldelo.com>
2025-03-03 21:21:04 -0800

    Cleanup duplicate model code
    remove duplicate api structs

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,30 +12,3 @@ type User struct {
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
-
-// CreateUserRequest represents the request to create a new user
-type CreateUserRequest struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-}
-
-// CreateUserResponse represents the response after creating a user
-type CreateUserResponse struct {
-	User User `json:"user"`
-}
-
-// GetUserResponse represents the response when fetching a user
-type GetUserResponse struct {
-	User User `json:"user"`
-}
-
-// ListUsersResponse represents the response when listing users
-type ListUsersResponse struct {
-	Users []User `json:"users"`
-}
-
-// UpdateUserRequest represents the request to update a user
-type UpdateUserRequest struct {
-	Name  string `json:"name,omitempty"`
-	Email string `json:"email,omitempty"`
-}
